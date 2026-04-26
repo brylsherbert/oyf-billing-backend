963758f214a5806d273e6680c5874e434b2b1fe0
Bryl Magdadaro <asapsherbs@gmail.com>
2026-04-26 14:04:47 +0800

    build: add support for DATABASE_URL

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,24 @@
 import pkg from "pg";
 import { config } from "dotenv";
+
 const { Pool } = pkg;
 
 config();
 
-const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+    });
 
 const connectDB = async () => {
   let retries = 5;
